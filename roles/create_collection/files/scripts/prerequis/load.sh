52a5d9a44bb9369f633d897519b3630612bf5255
MozeBaltyk <morze.baltyk@proton.me>
2023-08-24 20:59:56 +0200

    update scripts

--- roles/create_collection/files/scripts/prerequis/load.sh
+++ roles/create_collection/files/scripts/prerequis/load.sh
@@ -1,42 +1,57 @@
 #!/bin/bash
+set -eo pipefail
 
-# load images listed in deps/images.txt
-printf "\e[1;34m[INFO]\e[m load images listed in meta/ee-images.tx.\n"
+# Helm and yq are required
+check_requirement(){
+helm version
+yq --version
+}
 
-for line in $(cat ../../meta/ee-images.txt); do
-  echo "### ${line} ###"
-  image=$( echo "${line}" | awk  -F ";" '{print $1}' )
-  dir=$( echo "${line}" | awk  -F ";" '{print $2}' )
-  archive_name=$(basename -a $(awk -F : '{print $1}'<<<$image));
-  echo "### Pull images ###"
-  podman pull ${image}
-  echo "### Push images in dir ${dir} ###"
-  mkdir -p ../../$dir
-  podman save ${image} --format oci-archive -o ../../$dir/$archive_name;
-done
+load_helm_images(){
+  # look in helm charts
+  for helm in $(ls ../../roles/*/files/helm/*.tgz); do
+    printf "\e[1;34m[INFO]\e[m Look for images in ${helm}...\n"
 
-# load images from helm charts
-for helm in $(ls ../../roles/*/files/helm/*.tgz); do
-  printf "\e[1;34m[INFO]\e[m Look for images in ${helm}...\n"
+    images=$(helm template -g $helm |yq -N '..|.image? | select(.)'|sort|uniq|grep ":"|egrep -v '*:[[:blank:]]' || echo "")
 
-  images=$(helm template -g $helm |yq -N '..|.image? | select(.)'|sort|uniq|grep ":"|egrep -v '*:[[:blank:]]')
+    dir=$( dirname $helm | xargs dirname )
 
-  dir=$( dirname $helm | xargs dirname )
+    echo "####"
 
-  echo "####"
+    if [ "$images" != "" ]; then
+      printf "\e[1;34m[INFO]\e[m Images found in the helm charts: ${images}\n"
+      printf "\e[1;34m[INFO]\e[m Create ${dir}/images images...\n"
 
-  if [ "$images" != "" ]; then
-    printf "\e[1;34m[INFO]\e[m Images found in the helm charts: ${images}\n"
-    printf "\e[1;34m[INFO]\e[m Create ${dir}/images images...\n"
+      mkdir -p ${dir}/images
 
-    mkdir -p ${dir}/images
+      while i= read -r image_name; do
+        archive_name=$(basename -a $(awk -F : '{print $1}'<<<${image_name}));
+        printf "\e[1;34m[INFO]\e[m Pull images...\n"
+        podman pull ${image_name};
+        printf "\e[1;34m[INFO]\e[m Push ${image_name} in ${dir}/images/${archive_name}\n"
+        podman save ${image_name} --format oci-archive -o ${dir}/images/${archive_name};
+      done <<< ${images}
+    else
+      printf "\e[1;34m[INFO]\e[m No Images found in the helm charts: $helm\n"
+    fi
+  done
+}
 
-    while i= read -r image_name; do
-      archive_name=$(basename -a $(awk -F : '{print $1}'<<<${image_name}));
-      printf "\e[1;34m[INFO]\e[m Pull images...\n"
-      podman pull ${image_name};
-      printf "\e[1;34m[INFO]\e[m Push ${image_name} in ${dir}/images/${archive_name}\n"
-      podman save ${image_name} --format oci-archive -o ${dir}/images/${archive_name};
-    done <<< ${images}
-  fi
-done
+load_images(){
+  # look in meta/ee-images.txt
+  for line in $(cat  ../../meta/ee-images.txt); do
+    echo "### ${line} ###"
+    image=$( echo "${line}" | awk  -F ";" '{print $1}' )
+    dir=$( echo "${line}" | awk  -F ";" '{print $2}' )
+    archive_name=$(basename -a $(awk -F : '{print $1}'<<<$image));
+    printf "\e[1;34m[INFO]\e[m Pull images...\n"
+    podman pull ${image}
+    printf "\e[1;34m[INFO]\e[m Push ${image_name} in ${dir}/images/${archive_name}\n"
+    mkdir -p  ../../$dir
+    podman save ${image} --format oci-archive -o ../../$dir/$archive_name;
+  done
+}
+
+check_requirement
+load_helm_images
+load_images
--- scripts/prerequis/arkade.sh
+++ scripts/prerequis/arkade.sh
@@ -10,14 +10,29 @@ find_home_profile(){
 }
 
 # Install Arkade
-install_arkade() {
+install_arkade(){
   LATEST_VERSION_URL="https://api.github.com/repos/alexellis/arkade/releases/latest"
   LATEST_VERSION=$(curl -s "${LATEST_VERSION_URL}" | grep '"tag_name":' | cut -d'"' -f4)
   DOWNLOAD_URL="https://github.com/alexellis/arkade/releases/download"
+
+  if ! grep -qF "export PATH=\$PATH:\$HOME/.arkade/bin" "$HOME_PROFILE"; then
+      printf "\e[1;33m[CHANGE]\e[m Appending arkade bin path to $HOME_PROFILE ...\n"
+      mkdir -p $HOME/.arkade/bin
+      echo "export PATH=\$PATH:\$HOME/.arkade/bin" >> "$HOME_PROFILE"
+      echo "Export new PATH"
+      export PATH=$PATH:$HOME/.arkade/bin
+      #source $HOME_PROFILE
+      echo ""
+  else
+      printf "\e[1;34m[INFO]\e[m ~/.arkade/bin path is already present in $HOME_PROFILE.\n"
+      echo ""
+  fi
+
   if [[ ! -f /usr/local/bin/arkade ]]; then
       printf "\e[1;33m[CHANGE]\e[m arkade is not found. Installing...\n"
       curl -LO "${DOWNLOAD_URL}"/"${LATEST_VERSION}"/arkade > /dev/null 2>&1
-      chmod +x arkade && sudo mv arkade /usr/local/bin/
+      chmod +x arkade
+      cp arkade $HOME/.arkade/bin
       CURRENT_VERSION=$(arkade version | grep 'Version:' | awk '{ print $2 }')
       printf "\e[1;32m[OK]\e[m arkade $CURRENT_VERSION has been installed.\n"
   else
@@ -27,30 +42,25 @@ install_arkade() {
       if [[ "$CURRENT_VERSION" != "$LATEST_VERSION" ]]; then
           printf "\e[1;33m[CHANGE]\e[m New version of arkade found, current: $CURRENT_VERSION. Updating...\n"
           curl -LO "${DOWNLOAD_URL}"/"${LATEST_VERSION}"/arkade > /dev/null 2>&1
-          chmod +x arkade && sudo mv arkade /usr/local/bin/
+          chmod +x arkade
+          cp arkade $HOME/.arkade/bin
           printf "\e[1;32m[OK]\e[m arkade has been updated to version $CURRENT_VERSION.\n"
       else
           CURRENT_VERSION=$(arkade version | grep 'Version:' | awk '{ print $2 }')
           printf "\e[1;34m[INFO]\e[m arkade $CURRENT_VERSION is up-to-date.\n"
       fi
   fi
-
-  find_home_profile
-
-  if ! grep -qF "export PATH=\$PATH:\$HOME/.arkade/bin" "$HOME_PROFILE"; then
-      printf "\e[1;33m[CHANGE]\e[m Appending arkade bin path to .zshrc...\n"
-      echo "export PATH=\$PATH:\$HOME/.arkade/bin" >> "$HOME_PROFILE"
-  else
-      printf "\e[1;34m[INFO]\e[m ~/.arkade/bin path is already present in $HOME_PROFILE.\n"
-  fi
 }
 
 #Install arkade packages
-install_pkg(){ 
-  for line in $(cat ../../deps/arkade.txt | egrep -v "#|^$" ); do
-    arkade get ${line}
+install_pkg(){
+  for line in $(cat ../../meta/ee-arkade.txt | egrep -v "#|^$" ); do
+    arkade get --progress=false ${line} > /dev/null
   done
+  printf "\e[1;34m[INFO]\e[m Following packages were installed in ~/.arkade/bin : \n"
+  ls -l $HOME/.arkade/bin
 }
 
+find_home_profile
 install_arkade
 install_pkg
